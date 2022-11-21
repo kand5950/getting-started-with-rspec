b7ddecd9ba90afb5c40dfa5270c5b9d0a29838b8
Anit <anittankandasamy@gmail.com>
2022-11-21 14:48:49 -0500

    write the minimum amount of code to make spec pass

--- StringCalculator/spec/string_calculator_spec.rb
+++ StringCalculator/spec/string_calculator_spec.rb
@@ -1,4 +1,12 @@
 require 'string_calculator'
 
 describe StringCalculator do 
+  #add class method: given an empty string, it returns zero
+  describe ".add" do #.add class method #add instance method
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
 end
